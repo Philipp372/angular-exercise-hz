fdb9f0d95a45c83ed9430a54e9183038c5758ea0
Philipp Rakuschan <prakuschan@psi.at>
2020-04-17 07:41:08 +0200

    version 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,23 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { ListGithubReposComponent } from './list-github-repos/list-github-repos.component';
+import { HeaderComponent } from './header/header.component';
+import { FilterPipe } from './list-github-repos/filter.pipe';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    ListGithubReposComponent,
+    HeaderComponent,
+    FilterPipe,
+  ],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule],
+  providers: [],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
